e4de676e289b3113202f9af7d2f85fcc6b182587
Brandon Gonzalez <brajgon@gmail.com>
2024-04-11 19:31:39 -0600

    final commit

--- code.js
+++ code.js
@@ -1,7 +1,3 @@
-
-// Work through the following problems in Javascript (you have seen these problems before). Create a new file for your code. When you have finished with each function, leave a code comment with what you believe the runtime of your code to be.
-
-
 // Write a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
 function isSumZero(nums){
     let numSet = new Set();
@@ -18,7 +14,7 @@ const array1 = [1, 2, 3, 4, 5];
 console.log(isSumZero(array1));
 const array2 = [1, 2, 3, -3];
 console.log(isSumZero(array2));
-// O(n)
+// 7 ms
 
 // Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
 
@@ -35,8 +31,7 @@ function hasUniqueCharacters(word) {
 
 console.log(hasUniqueCharacters("hello"));
 console.log(hasUniqueCharacters("world"));
-
-// O(n^2)
+// 5 ms
 
 // A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”Write a function to check a sentence to see if it is a pangram or not.
 
@@ -52,4 +47,21 @@ function isPangram(sentence) {
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog."));
 console.log(isPangram("Norm Mcdonald would have loved today so much"));
-// O(n)
+// 10 ms
+
+// write a function, find_longest_word, that takes a list of words and returns the length of the longest one
+
+function find_longest_word(words) {
+    let longest = 0;
+    for (let word of words) {
+        if (word.length > longest) {
+            longest = word.length;
+        }
+    }
+    return longest;
+}
+
+const thisArray = ["Brandon", "Gonzalez", "Likes", "To", "Code"];
+console.log(find_longest_word(thisArray));
+
+// 16 ms
